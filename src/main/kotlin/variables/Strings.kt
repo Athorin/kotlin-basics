4541090c173db0ce2301622dfff6ed28e0560dd6
Fran Azorin <40206025+Pakychoko@users.noreply.github.com>
2022-11-29 17:15:43 +0100

    v0.0.2 Strings variables

---
+++ src/main/kotlin/variables/Strings.kt
@@ -0,0 +1,27 @@
+package variables
+
+class Strings {
+
+    var name: String = "Fran" // common variable that can be changed
+    val surname: String = "Azorin" // val (Value) -> immutable variable, cannot be changed once the value has been created
+
+    // Its prefer to use val wherever possible (reason why var is underlined)
+
+    // Variables dont need to define a type (but kotlin is strongly typed language)
+    var comment = "My comment" // so this is created as string
+
+    // Multiline string
+    val story = """It was a dark a storm night.
+        |A foul smell crept across the city.
+        |Jane wondered that time it was, and when it would be daylight."""
+        .trimMargin("|") // (pipe indicate where margin stops to fit de line)
+
+    fun printName() {
+        // This is a string template
+        println("Hello $name ${surname.uppercase()}")
+        println("The \$name variable has ${name.length} characters")
+        println("Your product cost $10 or \$about10") // \$ is putted to avoid take as varible
+
+        println(story)
+    }
+}
