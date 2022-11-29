15f307f69284cf6c6a98ca1a5d410b3b55296044
Fran Azorin <40206025+Pakychoko@users.noreply.github.com>
2022-11-29 17:14:36 +0100

    v.0.0.1 Base created

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+import variables.Strings as StringsAlias
+
+fun main(args: Array<String>) {
+    println("Hello World! Its a learning course!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+
+    Strings.
+}
